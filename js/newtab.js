b227be4f9736e570d0ac82326921de1ec97c154b
Miguel Zepeda <miguelzepeda350@gmail.com>
2023-09-03 14:24:31 -0400

    Implements basic time functionality

--- js/newtab.js
+++ js/newtab.js
@@ -1,3 +1,17 @@
+function updateCurrentTime() {
+    const element = document.getElementById('current-time');
+    setInterval(function () {
+      const currentDate = new Date();
+      let hours = currentDate.getHours()
+      let minutes = currentDate.getMinutes();
+      if(hours > 12){
+        hours -= 12;
+      }
+      if(minutes < 10){
+        minutes = '0' + minutes;
+      }
+      element.innerText = hours + ":" + minutes;
+    }, 1000);
+  }
 
-
-  
+updateCurrentTime();
