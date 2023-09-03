2b122d3628780efc26bc603d4289ac682d685480
Miguel Zepeda <miguelzepeda350@gmail.com>
2023-09-03 19:20:43 -0400

    Added hovering functionality

--- js/newtab.js
+++ js/newtab.js
@@ -1,17 +1,47 @@
-function updateCurrentTime() {
-    const element = document.getElementById('current-time');
-    setInterval(function () {
-      const currentDate = new Date();
-      let hours = currentDate.getHours()
-      let minutes = currentDate.getMinutes();
-      if(hours > 12){
-        hours -= 12;
-      }
-      if(minutes < 10){
-        minutes = '0' + minutes;
-      }
-      element.innerText = hours + ":" + minutes;
-    }, 1000);
-  }
+const timeElement = document.getElementById('current-time');
+let hovering = false;
 
-updateCurrentTime();
+// Function to update the time
+function updateTime() {
+    const currentDate = new Date();
+    let hours = currentDate.getHours(), minutes = currentDate.getMinutes(), seconds = currentDate.getSeconds();
+    let am_pm = "a.m";
+
+    //Updates time for 12hr standard and AM/PM
+    if (hours > 12) {
+      hours -= 12;
+      am_pm = "p.m";
+    }
+    else if(hours == 12){
+      am_pm = "p.m";
+    }
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+
+    //Check if hovering, and display seconds accordingly
+    if (hovering) {
+      timeElement.innerText = hours + ":" + minutes + ":" + seconds + " " + am_pm;
+    } else {
+      timeElement.innerText = hours + ":" + minutes + " " + am_pm;
+    }
+}
+//Updates time initially
+updateTime();
+
+//Updates time every second
+setInterval(updateTime, 1000);
+
+//Event listeners to detect mouse hovering
+timeElement.addEventListener('mouseenter', function () {
+  hovering = true;
+  updateTime(); 
+});
+
+timeElement.addEventListener('mouseleave', function () {
+  hovering = false;
+  updateTime();
+});
