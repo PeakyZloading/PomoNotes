eeac080fa63e16d4cd859e2186d65a26e2903975
Miguel Zepeda <miguelzepeda350@gmail.com>
2023-09-26 20:15:48 -0400

    Improves styling of the sticky note and delete functionality

--- js/stickyNotes.js
+++ js/stickyNotes.js
@@ -1,28 +1,65 @@
 const stickyNoteElement = document.getElementById('stickyNoteButton');
 
-//Track the currently dragged sticky note
-let currentStickyNote; 
+let currentStickyNote; // Track the currently dragged sticky note
 
 stickyNoteElement.addEventListener('click', function () {
-
-  //Creates the structure for the sticky notes
+  // Creates the structure for the sticky notes
   const stickyNoteBox = document.createElement('div');
-  stickyNoteBox.style.height = '200px';
   stickyNoteBox.style.width = '200px';
   stickyNoteBox.style.backgroundColor = 'yellow';
   stickyNoteBox.style.position = 'absolute'; // Make it positioned
   stickyNoteBox.style.cursor = 'move'; // Set cursor style to indicate draggability
 
+  // Create the top part for the delete button
+  const topPart = document.createElement('div');
+  topPart.style.height = '20px'; // Height for the top part
+  topPart.style.backgroundColor = 'red'; // Background color for the top part
+
+  //Create the delete button
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = 'X';
+  deleteButton.style.cursor = 'pointer';
+  deleteButton.style.backgroundColor = 'transparent';
+  deleteButton.style.border = 'none';
+  deleteButton.style.fontSize = '14px';
+  deleteButton.style.color = 'white';
+
+  //Position the delete button in the top part
+  deleteButton.style.position = 'absolute';
+  deleteButton.style.top = '2px';
+  deleteButton.style.right = '5px';
+
+  deleteButton.addEventListener('click', () => {
+    //Removes the sticky note
+    document.body.removeChild(stickyNoteBox);
+  });
+
+  //Create the bottom part for the editable content
+  const bottomPart = document.createElement('div');
+  bottomPart.style.padding = '10px';
+  bottomPart.style.overflow = 'auto';
+  bottomPart.style.height = '150px'; 
+  bottomPart.style.borderTop = '1px solid black';
+
   //Sets up editable text for sticky note
   const editableContent = document.createElement('div');
-  editableContent.style.width = '180px';
-  editableContent.style.height = '180px';
+  editableContent.style.width = '100%';
+  editableContent.style.height = '100%';
   editableContent.style.border = 'none';
-  editableContent.style.padding = '10px';
   editableContent.style.overflow = 'auto';
+  editableContent.style.outline = 'none';
   editableContent.setAttribute('contentEditable', 'true');
   editableContent.textContent = 'Write your note here...';
-  stickyNoteBox.appendChild(editableContent);
+
+  //Append the delete button to the top part
+  topPart.appendChild(deleteButton);
+
+  //Append the top and bottom parts to the sticky note
+  stickyNoteBox.appendChild(topPart);
+  stickyNoteBox.appendChild(bottomPart);
+  
+  //Append the editable content to the bottom part
+  bottomPart.appendChild(editableContent);
 
   document.body.appendChild(stickyNoteBox);
 
@@ -31,7 +68,8 @@ stickyNoteElement.addEventListener('click', function () {
 
   // Event listener for drag start
   stickyNoteBox.addEventListener('dragstart', (e) => {
-    currentStickyNote = stickyNoteBox;
+    //Track the currently dragged sticky note
+    currentStickyNote = stickyNoteBox; 
   });
 });
 
@@ -46,11 +84,11 @@ document.addEventListener('drop', (e) => {
   if (currentStickyNote) {
     const x = e.clientX - currentStickyNote.clientWidth / 2;
     const y = e.clientY - currentStickyNote.clientHeight / 2;
-    
+
     //Move the currently dragged sticky note to the drop location
     currentStickyNote.style.left = x + 'px';
     currentStickyNote.style.top = y + 'px';
     //Reset the currently dragged sticky note
-    currentStickyNote = null;
+    currentStickyNote = null; 
   }
 });
